f3c2200cb3265e053c2e8619edf82a58cfa640c4
rajat-chauhan <rajc87417@gmail.com>
2022-06-26 15:52:09 +0530

    update

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let arrow1 = document.getElementById("arrow1");
 let arrow2 = document.getElementById("arrow2");
 let arrow3 = document.getElementById("arrow3");
 let arrow4 = document.getElementById("arrow4");
+let arrow5 = document.getElementById("arrow5");
 
 let btn1 = document.getElementById("btn1");
 let btn2= document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
 
 arrow1.addEventListener('click', function(){
     btn1.click();
@@ -23,6 +25,11 @@ arrow3.addEventListener('click', function(){
 arrow4.addEventListener('click', function(){
     btn4.click();
 })
+
+arrow5.addEventListener('click', function(){
+    btn5.click();
+})
+
 //JS smooth scrolling to anchor
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
